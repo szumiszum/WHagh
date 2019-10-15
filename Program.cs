c23b6f194560b5140cab9621f08db6cb79f8d953
szumiszum <37143243+szumiszum@users.noreply.github.com>
2019-10-15 11:13:45 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, i, j, k;
+            Console.WriteLine("Podaj rozmiar tabliczki mnozenia: ");
+            a = int.Parse(Console.ReadLine());
+            for (j = 1; j <= a; j++)
+            {
+                if (j == 1)
+                {
+                    Console.Write("");
+                    Console.Write("\t{0}", j);
+                }
+                else
+                {
+                    Console.Write("\t{0}", j);
+
+                }
+            }
+            for (i = 1; i <= a; i++)
+            {
+                Console.Write("\n {0}", i);
+                for (k = 1; k <= a; k++)
+                {
+                    b = i * k;
+                    Console.Write("\t{0}", b);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
